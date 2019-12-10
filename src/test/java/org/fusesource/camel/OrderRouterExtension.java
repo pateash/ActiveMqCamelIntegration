9096a90f533bbc65896d5379de8cf566153e2531
ashish <ashishpatel0720@gmail.com>
2019-12-11 00:38:34 +0530

    tests working

--- src/test/java/org/fusesource/camel/OrderRouterExtension.java
+++ src/test/java/org/fusesource/camel/OrderRouterExtension.java
@@ -4,8 +4,8 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class OrderRouterExtension extends RouteBuilder {
     @Override
-    public void configure() throws Exception {        
-        from("jms:orders").to("mock:orders");
+    public void configure() throws Exception {
+        from("jms:orders").to("mock:orders"); // comment this if we want to see data in ActiveMQ, as this will dequeue
     }
 
 }
--- src/test/java/org/fusesource/camel/PlaceOrderTest.java
+++ src/test/java/org/fusesource/camel/PlaceOrderTest.java
@@ -33,39 +33,39 @@ public class PlaceOrderTest extends CamelSpringTestSupport {
         // Try placing two file orders
         MockEndpoint result = getMockEndpoint("mock:orders");
         result.reset();
-        result.expectedMessageCount(2);
-        
+        result.expectedMessageCount(2); // set expected output should be come, will be checked in assertIsSatisfied
+
         // clear out any old orders
         deleteDirectory("target/placeorder");
-        IOHelper.mkdirs(new File("target/placeorder"));      
-        
+        IOHelper.mkdirs(new File("target/placeorder"));
+
         // place 2 file based orders
         IOHelper.copyFile(new File("src/data/message1.xml"), new File("target/placeorder/message1.xml"));
         IOHelper.copyFile(new File("src/data/message2.csv"), new File("target/placeorder/message2.csv"));
-        
+
         result.assertIsSatisfied();
 
-        // Now try placing an order via HTTP       
-        result.reset();
+        // Now try placing an order via HTTP
+        result.reset(); //reset everything
         result.expectedMessageCount(1);
         result.expectedBodiesReceived(new Order("gearbox", 5));
-        
-        String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" 
+
+        String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
             + "<order name=\"gearbox\" amount=\"5\"/>";
         Object response = template.requestBody("http://localhost:8888/placeorder", body);
         // convert the response to a String
         String responseString = context.getTypeConverter().convertTo(String.class, response);
         assertEquals("OK", responseString);
-        
+
         // ensure that the order got through to the mock endpoint
         result.assertIsSatisfied();
-    }       
+    }
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
         return new OrderRouterExtension();
     }
-    
+
     @Override
     protected AbstractXmlApplicationContext createApplicationContext() {
         return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
